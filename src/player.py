926bc055304bd82a1e7eafd13b50a345b86ceff0
jwross24 <jwross24@users.noreply.github.com>
2020-05-07 15:51:44 -0400

    implement Player and Room classes

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
+from room import Room
+
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name: str, current_room: Room):
+        self.name = name
+        self.current_room = current_room
--- src/room.py
+++ src/room.py
@@ -1,2 +1,19 @@
+from room import Room
+
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    # Instantiate the {direction}_to attributes with None if they are not
+    # specifically passed into the constructor
+    def __init__(
+        self, name: str, description: str, n_to: Room = None,
+        s_to: Room = None, e_to: Room = None, w_to: Room = None
+    ):
+        self.name = name
+        self.description = description
+        self.n_to = n_to
+        self.s_to = s_to
+        self.e_to = e_to
+        self.w_to = w_to
