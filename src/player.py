aa15be1e7952aeaa8e6bc0d85b49336fe01a375f
jwross24 <jwross24@users.noreply.github.com>
2020-05-07 16:06:07 -0400

    adjust type hinting for classes

--- src/player.py
+++ src/player.py
@@ -5,6 +5,6 @@ from room import Room
 
 
 class Player:
-    def __init__(self, name: str, current_room: Room):
+    def __init__(self, name: str, current_room: Room) -> None:
         self.name = name
         self.current_room = current_room
--- src/room.py
+++ src/room.py
@@ -6,9 +6,9 @@ class Room:
     # Instantiate the {direction}_to attributes with None if they are not
     # specifically passed into the constructor
     def __init__(
-        self, name: str, description: str, n_to=None, s_to=None, e_to=None,
-        w_to=None
-    ):
+            self, name: str, description: str, n_to=None, s_to=None,
+            e_to=None, w_to=None
+    ) -> None:
         self.name = name
         self.description = description
         self.n_to = n_to
